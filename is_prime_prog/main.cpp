8d5799f63c08e7d9d3e734c4cc8cd2e2f3ad5c38
Nathan RABET <nathan.rabet@epita.fr>
2022-09-25 10:01:25 +0200

    is_prime_prog

--- is_prime_prog/main.cpp
+++ is_prime_prog/main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
-{}
+{
+    int number;
+    while (std::cin >> number)
+    {
+        int is_prime = 0;
+        cout << number << " is a prime: ";
+        for (int i = 2; i <= number / 2; ++i)
+            if (number % i == 0)
+                is_prime = 1;
+
+        if (is_prime == 1)
+            cout << "False" << endl;
+
+        else
+            cout << "True" << endl;
+    }
+}
